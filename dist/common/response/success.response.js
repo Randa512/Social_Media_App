9bd938066919ef5f2e977df8c430ff6bc7a66378
Randa512 <randahussien512@gmail.com>
2026-04-16 21:43:20 +0200

    remove node_modules and dist from tracking

--- dist/common/response/success.response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.successResponse = void 0;
-const successResponse = ({ res, message = "Done", status = 200, data }) => {
-    return res.status(status).json({ message, status, data });
-};
-exports.successResponse = successResponse;
